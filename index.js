133bc8701a04ec1bfa665aec5403d88e49bea445
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-11-13 19:32:23 -0600

    Completed required functions and recieved output in dist folder

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description of your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+        default: 'npm i -y'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'testing',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the Repo?',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Insert link to Mock-up if needed:',
+        name: 'mock-up',
+        default: './assets/images/'
+    },
+    {
+        type: 'input',
+        message: 'Insert your application URL:',
+        name: 'link-to-app'
+    },
+    {
+        type: 'input',
+        message: 'What kind of license would you like to use?',
+        name: 'license'
+    },
+    {
+        type: 'input', 
+        message: 'What is your GitHub username?',
+        name: 'GitHub'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    const fileName = "./dist/README.md";
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Your README.md has been created')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => writeToFile(generateMarkdown(answers)))
+    }
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,96 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+
+  if(license != "None") {
+    badge = "![License Badge](https://shields.io/badge/license-" + license + "-green)";
+  }
+
+  return badge;
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+  switch(license) {
+    case "MIT":
+      licenseLink = "https://mit-license.org/";
+      break;
+    case "BSD":
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    case "GPL": 
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+      break;
+    default:
+      licenseLink = "";
+      break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license){
+  let licenseSection = "";
+
+  if (license != "None") {
+    licenseSection += "## License\n"
+    licenseSection += "Please see " + renderLicenseLink(license) + " to get info about this license\n";
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## license
+    ${data.license}
+    
+  ## Table of Contents
+    * [Description](#description)
+        <a name = "Description"></a>
+    * [installation](#installation)
+        <a name = "Installation"></a>
+    * [testing](#testing)
+        <a name = "Testing"></a>
+    * [contribution](#contribution)
+        <a name = "Contribution"></a>
+    * [Mock-Up](#mock-up)
+        <a name = "Mock-Up"></a>
+    * [Application](#link-to-app)
+        <a name = "Application"></a>
+    * [license](#license)
+        <a name = "License"></a
+    * [email](#email)
+        <a name = "Email"></a
+    * [GitHub](#GitHub)
+        <a name = "GitHub"></a>
+    
+  ## description
+    ${data.description}
+    
+  ## installation
+    ${data.installation}
+    
+  ## testing
+    ${data.testing}
+    
+  ## contribution
+    ${data.contribution}
+
+  ## Mock-Up
+
+  ## Contact Me  
+    ### email
+      ${data.email}
+  
+    ### GitHub
+      ${data.GitHub}`;  
 }
 
 module.exports = generateMarkdown;
